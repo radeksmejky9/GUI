32f72a221c7551c458185d04e2d1cde342a94868
Helmanz <helcze4@gmail.com>
2024-05-08 00:30:21 +0200

    edited layout

--- TodoList/Pages/Index.cshtml.cs
+++ TodoList/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TodoList.Pages
@@ -6,15 +5,33 @@ namespace TodoList.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public int RemainingDays { get; private set; }
+        public string TextColor { get; private set; }
+        public bool DisplayAddTask { get; private set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            TextColor = "black";
         }
 
         public void OnGet()
         {
-
+        }
+        public int GetRemainingDays(int i)
+        {
+            DateTime deadlineDate = DateTime.Now.AddDays(i);
+            return (deadlineDate - DateTime.Now).Days;
+        }
+        public void OnPostAddTask()
+        {
+            if (DisplayAddTask)
+            {
+                DisplayAddTask = false;
+            } else
+            {
+                DisplayAddTask = true;
+            }
         }
     }
 }
